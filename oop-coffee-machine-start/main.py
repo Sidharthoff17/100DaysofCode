afaa0a5c016894941e9db55f27730bff071c7295
Sidharthoff17 <150654540+Sidharthoff17@users.noreply.github.com>
2025-06-29 23:05:09 +1000

    completion of Coffee Machine project with OOP

--- oop-coffee-machine-start/main.py
+++ oop-coffee-machine-start/main.py
@@ -1,3 +1,49 @@
 from menu import Menu
 from coffee_maker import CoffeeMaker
 from money_machine import MoneyMachine
+def user_order() -> str:
+    while True:
+        try:
+            coffeeType = input("What is your order(espresso/latte/cappuccino):").strip()
+            if coffeeType in ["espresso", "latte", "cappuccino", "off", "report"]:
+                break
+            raise ValueError
+        except ValueError:
+            pass
+
+    return coffeeType
+
+def main():
+    #create cafe menu object
+    cafe_menu = Menu()
+    cafe_money_machine = MoneyMachine()
+    cafe_coffee_maker = CoffeeMaker()
+
+    #get the drink that is being ordered
+    while True:
+        #get cafe order
+        drink_order = user_order()
+
+        if drink_order == "off":
+            break
+        elif drink_order == "report":
+            cafe_coffee_maker.report()
+            cafe_money_machine.report()
+        else:
+            drink = cafe_menu.find_drink(drink_order)
+
+            #set checkers
+            ingredients_checker = cafe_coffee_maker.is_resource_sufficient(drink)
+            if not ingredients_checker:
+                break
+            money_checker = cafe_money_machine.make_payment(drink.cost)
+            if not money_checker:
+                break
+
+            if money_checker and ingredients_checker:
+                cafe_coffee_maker.make_coffee(drink)
+
+
+
+main()
+
