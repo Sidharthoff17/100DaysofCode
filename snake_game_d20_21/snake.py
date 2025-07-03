9948ab395ffe5591baef3ea97b4d449d6dc635e7
Sidharthoff17 <150654540+Sidharthoff17@users.noreply.github.com>
2025-07-03 22:59:12 +1000

    completion of snake game

--- snake_game_d20_21/snake.py
+++ snake_game_d20_21/snake.py
@@ -27,6 +27,21 @@ class Snake:
             self.segments[seg_num].goto(new_x, new_y)
         self.head.forward(20)
 
+    def add_part(self, position):
+
+
+        # Create a new segment at that position
+        new_segment = Turtle("square")
+        new_segment.color("white")
+        new_segment.penup()
+        new_segment.goto(position)
+
+        # Append it to the segments list
+        self.segments.append(new_segment)
+
+    def extend(self):
+        self.add_part(self.segments[-1].position())
+
     def up(self):
         if self.head.heading() != DOWN:
             self.head.setheading(90)
