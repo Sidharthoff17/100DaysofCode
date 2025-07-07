e7d3d1c42cf43dc30a0853bf7d95d9d286086237
Sidharthoff17 <150654540+Sidharthoff17@users.noreply.github.com>
2025-07-07 23:15:09 +1000

    commencement of day 31, flash card app

---
+++ flash-card-project-start/main.py
@@ -0,0 +1,16 @@
+from tkinter import  *
+import pandas as pd
+
+BACKGROUND_COLOR = "#B1DDC6"
+
+#create UI
+window = Tk()
+window.title("Flashy")
+window.minsize(width=1000, height= 650)
+window.config(padx=100, pady=50, bg=BACKGROUND_COLOR)
+
+
+
+
+window.mainloop()
+
