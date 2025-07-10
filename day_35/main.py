d4b6a084af325d0693373ef149066bf72c8dd8c0
Sidharthoff17 <150654540+Sidharthoff17@users.noreply.github.com>
2025-07-10 19:05:21 +1000

    checking if the next 4 hours predicts rains

--- day_35/main.py
+++ day_35/main.py
@@ -5,8 +5,22 @@ API_KEY = "ce89ee1a2f6f67562fd5b371f1957b8e"
 weather_params = {
     "lat": -37.813629,
     "lon": 144.963058,
-    "appid": API_KEY
+    "appid": API_KEY,
+    "cnt": 4,
 }
 
 response = requests.get(OWN_ENDPOINT, params= weather_params)
-print(response.json())
+response.raise_for_status()
+weather_data = response.json()
+
+#checker for rain
+will_rain = False
+
+for hour_data in weather_data["list"]:
+    condition_code = hour_data["weather"][0]["id"]
+    if int(condition_code) < 700:
+        will_rain = True
+
+if will_rain:
+    print("Bring an umbrella")
+
