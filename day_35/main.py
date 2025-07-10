a8f5db744e606d7c3bc959f4861b37651d0940dc
Sidharthoff17 <150654540+Sidharthoff17@users.noreply.github.com>
2025-07-10 22:59:24 +1000

    Completion of the twilio project

--- day_35/main.py
+++ day_35/main.py
@@ -1,6 +1,10 @@
 import requests
+from twilio.rest import Client
 OWN_ENDPOINT = "https://api.openweathermap.org/data/2.5/forecast"
 API_KEY = "ce89ee1a2f6f67562fd5b371f1957b8e"
+account_sid = "AC83c2563ea55ad901675bd3cc58e56156"
+auth_token= "3ab6f17272f58c2c1cc7d99fe34a92c3"
+client = Client(account_sid, auth_token)
 
 weather_params = {
     "lat": -37.813629,
@@ -22,5 +26,10 @@ for hour_data in weather_data["list"]:
         will_rain = True
 
 if will_rain:
-    print("Bring an umbrella")
-
+    client = Client(account_sid, auth_token)
+    message = client.messages.create(
+        body="It will rain in Melbourne Victoria in the next 4 hours",
+        from_="+16062120104",
+        to="+61401795190",
+    )
+    print(message.status)
